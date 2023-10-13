4e9211bbb77ff10e86a4b7f82a695bb134fddfc1
Regnun <kimzoo30@naver.com>
2023-10-13 22:40:56 +0900

    [Bronze I] Title: Balanced String, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/17520. Balanced String/Balanced String.cc
+++ 백준/Bronze/17520. Balanced String/Balanced String.cc
@@ -7,20 +7,14 @@ int main()
 	int n;
 	cin >> n;
 
-	vector<int> dp(n + 1);
-	dp[1] = 2;
+	int ans = 2;
 
 	for (int i = 2; i <= n; ++i)
 	{
 		if (i % 2)
 		{
-			dp[i] = 2 * dp[i - 1] % 16769023;
-		}
-		else
-		{
-			dp[i] = dp[i - 1];
-
+			ans = 2*ans % 16769023;
 		}
 	}
-	cout << dp[n];
+	cout << ans;
 }
