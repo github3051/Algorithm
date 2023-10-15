f4ba29246830e378185f302350b937e72998a3d4
Regnun <kimzoo30@naver.com>
2023-10-15 22:16:21 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 4 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    string str;
+    cin >> str;
+    
+    for(int i=n-5; i<n; ++i)
+    {
+        cout << str[i];
+    }
+}
