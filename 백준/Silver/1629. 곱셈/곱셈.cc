0a4118d801baed889adbd1811e9b60b7500e6221
Regnun <kimzoo30@naver.com>
2023-10-19 06:35:59 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -3,28 +3,21 @@ using namespace std;
 
 long long a, b, c;
 
-long long POW(long long a, long long b, long long m)
-{
-	if (b == 1)
-	{
-		return a % m;
-	}
-
-	long long val = POW(a, b / 2, m);
-	val = val * val % m;
-
-	if (b % 2 == 1)
-	{
-		return val * a % m;
-	}
-	else
-	{
-		return val;
-	}
-}
-
 int main()
 {
 	cin >> a >> b >> c;
-	cout << POW(a, b, c);
+
+	long long ans = 1;
+
+	while (b > 0)
+	{
+		if (b % 2 == 1)
+		{
+			ans = (ans * a) % c;
+		}
+		a = a * a % c;
+		b /= 2;
+	}
+
+	cout << ans;
 }
