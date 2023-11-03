09699fa50937ee444cc3fb59dd80011860c25b16
Regnun <kimzoo30@naver.com>
2023-11-03 22:18:11 +0900

    [Bronze II] Title: Bunnies, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26529. Bunnies/Bunnies.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    vector<int> dp(46);
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i=2; i<=45; ++i)
+    {
+        dp[i] = dp[i-2] + dp[i-1];
+    }
+    
+    for(int i=0; i<n; ++i)
+    {
+        int tmp;
+        cin >> tmp;
+        cout << dp[tmp] << '\n';
+    }
+}
