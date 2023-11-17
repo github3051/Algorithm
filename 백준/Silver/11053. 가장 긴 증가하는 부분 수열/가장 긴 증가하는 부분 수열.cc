0fc403ae0a16064b11b3b52a4133a1800d6fd6de
Regnun <kimzoo30@naver.com>
2023-11-18 00:08:12 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -3,29 +3,21 @@
 #include <algorithm>
 using namespace std;
 
-int arr[1001];
-int n;
+int a;
+vector<int> arr;
 
-int main()
-{
-	cin >> n;
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> arr[i];
-	}
+int main() {
+	cin >> a;
 
-	vector<int> dp(n, 1);
-
-	for (int i = 0; i < n; ++i) // 구하려는 dp의 위치(현재 위치)
-	{
-		for (int j = 0; j < i; ++j) // 구하려는 위치보다 앞에있는 값들 확인
-		{
-			if (arr[j] < arr[i]) // 증가 수열이기 때문에 현재 위치보다 앞에있는 값들은 현재 값보다 작아야됨
-			{
-				dp[i] = max(dp[i], dp[j]+1);
-			}
+	for (int i = 0; i < a; i++) {
+		int input;
+		cin >> input;
+		if (arr.empty() || arr.back() < input) {
+			arr.push_back(input);
+		}
+		else {
+			*lower_bound(arr.begin(), arr.end(), input) = input;
 		}
 	}
-	
-	cout << *max_element(dp.begin(), dp.end());
+	cout << arr.size();
 }
