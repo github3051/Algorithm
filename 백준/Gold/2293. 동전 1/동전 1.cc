e7e5b159237accc05de3f49deb8c2651b2f392d2
Regnun <kimzoo30@naver.com>
2023-11-23 21:11:09 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n,k;
+    cin >> n >> k;
+    vector<int> coin(n);
+    for(int i=0; i<n; ++i)
+    {
+        cin >> coin[i];
+    }
+    
+    vector<int> dp(k+1,0);
+    dp[0] = 1;
+    for(int i=0; i<n; ++i)
+    {
+        for(int j=coin[i]; j<=k; ++j)
+        {
+            dp[j] += dp[j-coin[i]];
+        }
+    }
+    cout << dp[k];
+}
