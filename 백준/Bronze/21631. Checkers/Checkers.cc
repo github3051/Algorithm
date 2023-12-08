a843a209e9de75107b25ba5522fdd8dd43b6fe45
Regnun <kimzoo30@naver.com>
2023-12-08 22:44:08 +0900

    [Bronze IV] Title: Checkers, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21631. Checkers/Checkers.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long w,b;
+    cin >> w >> b;
+    
+    if(w >= b)
+    {
+        cout << b;
+    }
+    else
+    {
+        cout << w+1;
+    }
+}
