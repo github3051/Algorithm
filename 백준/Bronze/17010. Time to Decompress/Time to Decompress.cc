eeea596f58bf4756f84dae0c2ccd3a7b57ba9c18
Regnun <kimzoo3051@naver.com>
2023-12-30 23:28:01 +0900

    [Bronze IV] Title: Time to Decompress, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17010. Time to Decompress/Time to Decompress.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    
+    while(tc--)
+    {
+        int n;
+        char c;
+        cin >> n >> c;
+        
+        for(int i=0; i<n; ++i)
+        {
+            cout << c;
+        }
+        cout << '\n';
+    }
+}
