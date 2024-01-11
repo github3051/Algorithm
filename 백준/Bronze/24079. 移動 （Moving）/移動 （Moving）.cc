9b9b0af9423a435bad4a3e7954849e34b1d3f6a3
Regnun <kimzoo3051@naver.com>
2024-01-11 19:32:29 +0900

    [Bronze IV] Title: 移動 (Moving), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24079. 移動 （Moving）/移動 （Moving）.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,z;
+    
+    cin >> a >> b >> z;
+    
+    if(a+b > z)
+    {
+        cout << 0;
+    }
+    else
+    {
+        cout << 1;
+    }
+}
