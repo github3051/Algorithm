4275f5057cd76395805ba039ffc0725a22a77685
Regnun <kimzoo3051@naver.com>
2024-01-17 23:22:16 +0900

    [Bronze IV] Title: 帰省 (Homecoming), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24072. 帰省 （Homecoming）/帰省 （Homecoming）.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (c < a || b <= c)
+    {
+        cout << 0;
+    }
+    else
+    {
+        cout << 1;
+    }
+}
