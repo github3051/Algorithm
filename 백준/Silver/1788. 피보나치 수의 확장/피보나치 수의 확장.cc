fcbadba7f987366186e3a94a7ea937814a390276
Regnun <kimzoo3051@naver.com>
2024-03-06 00:04:53 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.cc
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.cc
@@ -7,49 +7,40 @@ int main()
 	int n;
 	cin >> n;
 
-	bool isMinus = false;
-
-	if (n < 0)
-	{
-		isMinus = true;
-		n *= -1;
-	}
-
-	vector<int> dp(n + 2); // f(n) = f(n+1) + f(n+2);
-	vector<int> dpMinus(n + 2); // f(n) = f(n-2) - f(n-1);
-	dp[0] = 0;
-	dp[1] = 1;
-
-	dpMinus[0] = 0;
-	dpMinus[1] = 1; // n = -1
-
-	for (int i = 2; i <= n; ++i)
-	{
-		dp[i] = (dp[i - 1] + dp[i - 2]) % 1'000'000'000;
-	}
-
-	for (int i = 2; i <= n; ++i)
-	{
-		dpMinus[i] = (dpMinus[i - 2] - dpMinus[i - 1]) % 1'000'000'000;
-	}
-
 	if (n == 0)
 	{
 		cout << 0 << '\n' << 0;
 	}
-	else if(isMinus)
+	else if (n < 0)
 	{
-		if (dpMinus[n] < 0)
+		vector<int> dp(-n + 1);
+		dp[0] = 0;
+		dp[1] = 1;
+		for (int i = 2; i <= -n; ++i)
 		{
-			cout << -1 << '\n' << dpMinus[n]*-1;
+			dp[i] = (dp[i - 2] - dp[i - 1]) % 1'000'000'000;
+		}
+
+		if (dp[-n] < 0)
+		{
+			cout << -1 << '\n' << -dp[-n];
 		}
 		else
 		{
-			cout << 1 << '\n' << dpMinus[n];
+			cout << 1 << '\n' << dp[-n];
 		}
 	}
 	else
 	{
+		vector<int> dp(n + 1);
+		dp[0] = 0;
+		dp[1] = 1;
+		for (int i = 2; i <= n; ++i)
+		{
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 1'000'000'000;
+		}
+
 		cout << 1 << '\n' << dp[n];
 	}
+
 }
