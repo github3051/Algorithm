e270c67083406bd03ef56e2049ea63796880a71e
Regnun <kimzoo3051@naver.com>
2024-03-10 17:18:23 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 8 ms, Memory: 5928 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.cc
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.cc
@@ -1,17 +1,8 @@
 #include <iostream>
-#include <vector>
-using namespace std;
-
-int main()
-{
+int D[1000001]={0,1};
+int main(){
     int n;
-    vector<int> dp(n+1);
-    dp[1] = 0;
-    dp[2] = 1;
-    dp[3] = 1;
-    for(int i=4; i<=n; ++i)
-    {
-        dp[i] = (dp[i-1]+dp[i-2])%1'000'000'007;
-    }
-    cout << dp[n];
+    std::cin>>n;
+    for(int i=2;i<=n;i++)D[i]=(D[i-1]+D[i-2])%1000000007;
+    std::cout<<D[n];
 }
