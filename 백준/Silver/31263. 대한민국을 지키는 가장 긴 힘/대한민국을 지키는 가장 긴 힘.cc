d4d857435f9d3f3aafdb2b632a2a2175fb4e27ed
Regnun <kimzoo3051@naver.com>
2024-03-11 22:16:02 +0900

    [Silver III] Title: 대한민국을 지키는 가장 긴 힘, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.cc
+++ 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.cc
@@ -12,34 +12,16 @@ int main()
 	int ansCnt = 0;
 	for (int i = 0; i < n; ++i)
 	{
-		// 맨 앞자리가 0이라면 Cnt +1을 하고 i+1해서 다음으로 넘어감
-		if (str[i] == '0')
-		{
-			ansCnt += 1;
-			continue;
-		}
 
-		// 현재 위치에서 자릿수가 3개 이상 남아있다면
-		if (i + 3 <= n)
+		if ((i + 3 == n || (i + 3 < n && str[i + 3] != '0')) && stoi(str.substr(i, 3)) <= 641)
 		{
-			// 자릿수 3개의 값이 641 이하면 i를 ++3, 641 초과면 i를 ++2
-			int tmp = stoi(str.substr(i, 3));
-			if (tmp <= 641)
-			{
-				i += 2;
-			}
-			else
-			{
-				i += 1;
-			}
-			ansCnt += 1;
+			i += 2;
 		}
-		// 자릿수가 2개 이하라면
-		else
+		else if (i + 2 == n || (i + 2 < n && str[i + 2] != '0') && stoi(str.substr(i, 2)) <= 641)
 		{
-			ansCnt += 1;
-			break;
+			i += 1;
 		}
+		ansCnt += 1;
 	}
 
 	cout << ansCnt;
