b15af8fb53949bb44eb29d7b4023ca437f28e7ac
Regnun <kimzoo3051@naver.com>
2024-03-18 21:28:08 +0900

    [Silver II] Title: 민균이의 계략, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for(int i=0; i<n; ++i)
+    {
+        cin >> vec[i];
+    }
+    
+    vector<int> dp(n,1);
+    for(int i=1; i<n; ++i)
+    {
+        for(int j=0; j<i; ++j)
+        {
+            if(vec[i] > vec[j])
+            {
+                dp[i] = max(dp[i], dp[j]+1);
+            }
+        }
+    }
+    
+    cout << *max_element(dp.begin(),dp.end());
+}
