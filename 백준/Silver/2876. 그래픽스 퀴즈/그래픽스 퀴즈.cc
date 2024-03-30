b46404e925bbf73b39e94d946d71c6cabf4ac16e
Regnun <kimzoo3051@naver.com>
2024-03-31 00:08:51 +0900

    [Silver II] Title: 그래픽스 퀴즈, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2876. 그래픽스 퀴즈/그래픽스 퀴즈.cc
+++ 백준/Silver/2876. 그래픽스 퀴즈/그래픽스 퀴즈.cc
@@ -8,51 +8,31 @@ int main()
 	int n;
 	cin >> n;
 
-	vector<pair<int, int>> vec(n);
-	vector<vector<int>> dp(n, vector<int>(6, 0)); // dp[a][b] a번째 책상부터 시작할 때 b그레이드의 갯수
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> vec[i].first >> vec[i].second;
-		dp[i][vec[i].first] = 1;
-		dp[i][vec[i].second] = 1;
-	}
-
-	for (int i = 1; i < n; ++i)
-	{
-		if (vec[i - 1].first == vec[i].first || vec[i - 1].first == vec[i].second)
-		{
-			dp[i][vec[i - 1].first] = dp[i-1][vec[i-1].first] + 1;
-		}
-
-		if (vec[i - 1].second == vec[i].first || vec[i - 1].second == vec[i].second)
-		{
-			dp[i][vec[i - 1].second] = dp[i-1][vec[i - 1].second] + 1;
-		}
-	}
-
+	vector<int> grade(6, 0);
 	int ansMax = 0;
+	int ansIdx = 0;
 	for (int i = 0; i < n; ++i)
 	{
-		for (int j = 1; j <= 5; ++j)
+		int left, right;
+		cin >> left >>  right;
+		for (int j = 1; j <= 5; j++)
 		{
-			if (ansMax < dp[i][j])
+			if (j == left || j == right)
 			{
-				ansMax = dp[i][j];
+				grade[j] += 1;
+			}
+			else
+			{
+				grade[j] = 0;
+			}
+
+			if (grade[j] > ansMax)
+			{
+				ansMax = grade[j];
+				ansIdx = j;
 			}
 		}
 	}
 
-	vector<int> ansGrade;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 1; j <= 5; ++j)
-		{
-			if (ansMax == dp[i][j])
-			{
-				ansGrade.emplace_back(j);
-			}
-		}
-	}
-
-	cout << ansMax << ' ' << *min_element(ansGrade.begin(), ansGrade.end());
+	cout << ansMax << ' ' << ansIdx;
 }
