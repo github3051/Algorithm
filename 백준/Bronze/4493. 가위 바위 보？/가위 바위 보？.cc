61b5183c4f0d5e6cd1884764a767c40d648dc1d4
Regnun <kimzoo3051@naver.com>
2024-04-21 19:50:33 +0900

    [Bronze III] Title: 가위 바위 보?, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4493. 가위 바위 보？/가위 바위 보？.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        int n;
+        int p1Score = 0,p2Score = 0;
+        char p1,p2;
+        cin >> n;
+        
+        for(int i=0; i<n; ++i)
+        {
+            cin >> p1 >> p2;
+            if(p1 == 'R')
+            {
+                if(p2 == 'S')
+                {
+                    p1Score += 1;
+                }
+                else if(p2 == 'P')
+                {
+                    p2Score += 1;
+                }
+            }
+            else if(p1 == 'S')
+            {
+                if(p2 == 'R')
+                {
+                    p2Score += 1;
+                }
+                else if(p2 == 'P')
+                {
+                    p1Score += 1;
+                }
+            }
+            else if(p1 == 'P')
+            {
+                if(p2 == 'S')
+                {
+                    p2Score += 1;
+                }
+                else if(p2 == 'R')
+                {
+                    p1Score += 1;
+                }
+            }
+        }
+        if(p1Score == p2Score)
+        {
+            cout << "TIE\n";
+        }
+        else if(p1Score > p2Score)
+        {
+            cout << "Player 1\n";
+        }
+        else
+        {
+            cout << "Player 2\n";
+        }
+    }
+}
