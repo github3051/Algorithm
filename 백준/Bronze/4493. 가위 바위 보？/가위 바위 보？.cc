9d0e140716443ad91a6b80bc32a80bc5e0d6cc65
Regnun <kimzoo3051@naver.com>
2024-04-21 19:53:06 +0900

    [Bronze III] Title: 가위 바위 보?, Time: 52 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/4493. 가위 바위 보？/가위 바위 보？.cc
+++ 백준/Bronze/4493. 가위 바위 보？/가위 바위 보？.cc
@@ -16,38 +16,13 @@ int main()
         for(int i=0; i<n; ++i)
         {
             cin >> p1 >> p2;
-            if(p1 == 'R')
+            if( (p1 == 'R' && p2 == 'S' )||(p1 == 'S' && p2 == 'P' )||(p1 == 'P' && p2 == 'R' ))
             {
-                if(p2 == 'S')
-                {
-                    p1Score += 1;
-                }
-                else if(p2 == 'P')
-                {
-                    p2Score += 1;
-                }
+                p1Score += 1;
             }
-            else if(p1 == 'S')
+            else if( (p1 == 'R' && p2 == 'P' ) || (p1 == 'S' && p2 == 'R' ) || (p1 == 'P' && p2 == 'S' ))
             {
-                if(p2 == 'R')
-                {
-                    p2Score += 1;
-                }
-                else if(p2 == 'P')
-                {
-                    p1Score += 1;
-                }
-            }
-            else if(p1 == 'P')
-            {
-                if(p2 == 'S')
-                {
-                    p2Score += 1;
-                }
-                else if(p2 == 'R')
-                {
-                    p1Score += 1;
-                }
+                p2Score += 1;
             }
         }
         if(p1Score == p2Score)
