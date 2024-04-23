74f8ad158730fe5ace04f91b4c87258ae1ee9cb7
Regnun <kimzoo3051@naver.com>
2024-04-23 15:49:13 +0900

    [Bronze III] Title: 조교는 새디스트야!!, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14656. 조교는 새디스트야！！/조교는 새디스트야！！.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    int ans = 0;
+    for(int i=1; i<=n; ++i)
+    {
+        int tmp;
+        cin >> tmp;
+        if(tmp != i)
+        {
+            ans += 1;
+        }
+    }
+    cout << ans;
+}
