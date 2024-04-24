b90a6b15b61e2c527554e587d084077741c838d0
Regnun <kimzoo3051@naver.com>
2024-04-24 16:03:46 +0900

    [Bronze III] Title: J박스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5354. J박스/J박스.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        
+        for(int i=0;i<n;++i)
+        {
+            for(int j=0;j<n;++j)
+            {
+                if(i == 0 || i == n-1 || j == 0 || j == n-1)
+                {
+                    cout << '#';
+                }
+                else
+                {
+                    cout << 'J';
+                }
+            }
+            cout << '\n';
+        }
+        
+        cout << '\n';
+    }
+    
+}
