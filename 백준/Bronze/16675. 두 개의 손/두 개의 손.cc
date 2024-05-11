9223ea39a7f4add319c09df393f420b2c5feedd8
Regnun <kimzoo3051@naver.com>
2024-05-11 23:47:47 +0900

    [Bronze II] Title: 두 개의 손, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/16675. 두 개의 손/두 개의 손.cc
+++ 백준/Bronze/16675. 두 개의 손/두 개의 손.cc
@@ -1,3 +1,6 @@
+// MS가 같은 모양을 내는 경우 3가지와 TK가 같은 모양을 내는 경우 3가지 경우, 그 외 1개 총 7개의 경우만 구하면 된다.
+// SSPP같이 둘 다 겹치는 경우도 위에 포함된다.
+
 #include <iostream>
 using namespace std;
 
@@ -6,105 +9,32 @@ int main()
 	char ml, mr, tl, tr;
 	cin >> ml >> mr >> tl >> tr;
 
-	if (ml == mr)
+	if ((ml == 'R' && mr == 'R') && (tl == 'P' || tr == 'P'))
 	{
-		if (ml == 'R')
-		{
-			if (tl == 'P' || tr == 'P')
-			{
-				cout << "TK";
-			}
-			else if (tl == 'S' && tr == 'S')
-			{
-				cout << "MS";
-			}
-			else
-			{
-				cout << '?';
-			}
-		}
-		else if (ml == 'S')
-		{
-			if (tl == 'R' || tr == 'R')
-			{
-				cout << "TK";
-			}
-			else if (tl == 'P' && tr == 'P')
-			{
-				cout << "MS";
-			}
-			else
-			{
-				cout << '?';
-			}
-		}
-		else if (ml == 'P')
-		{
-			if (tl == 'S' || tr == 'S')
-			{
-				cout << "TK";
-			}
-			else if (tl == 'R' && tr == 'R')
-			{
-				cout << "MS";
-			}
-			else
-			{
-				cout << '?';
-			}
-		}
+		cout << "TK";
 	}
-	else if (tl == tr)
+	else if ((tl == 'R' && tr == 'R') && (ml == 'P' || mr == 'P'))
 	{
-		if (tl == 'R')
-		{
-			if (ml == 'P' || mr == 'P')
-			{
-				cout << "MS";
-			}
-			else if (ml == 'S' && mr == 'S')
-			{
-				cout << "TK";
-			}
-			else
-			{
-				cout << '?';
-			}
-		}
-		else if(tl == 'S')
-		{
-			if (ml == 'R' || mr == 'R')
-			{
-				cout << "MS";
-			}
-			else if (ml == 'P' && mr == 'P')
-			{
-				cout << "TK";
-			}
-			else
-			{
-				cout << '?';
-			}
-		}
-		else if (tl == 'P')
-		{
-			if (mr == 'S' || ml == 'S')
-			{
-				cout << "MS";
-			}
-			else if (ml == 'R' && mr == 'R')
-			{
-				cout << "TK";
-			}
-			else
-			{
-				cout << '?';
-			}
-		}
+		cout << "MS";			
+	}	
+	else if ((ml == 'S' && mr == 'S') && (tl == 'R' || tr == 'R'))
+	{
+		cout << "TK";
+	}
+	else if ((tl == 'S' && tr == 'S') && (ml == 'R' || mr == 'R'))
+	{
+		cout << "MS";
+	}
+	else if ((ml == 'P' && mr == 'P') && (tl == 'S' || tr == 'S'))
+	{				 			  
+		cout << "TK";
+	}				 			  
+	else if ((tl == 'P' && tr == 'P') && (ml == 'S' || mr == 'S'))
+	{
+		cout << "MS";
 	}
 	else
 	{
 		cout << '?';
 	}
-	
 }
