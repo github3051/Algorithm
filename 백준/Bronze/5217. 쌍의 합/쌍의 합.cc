e289587fd85e01a7d021062a151aad2b32cb5e65
Regnun <kimzoo3051@naver.com>
2024-05-29 22:58:25 +0900

    [Bronze III] Title: 쌍의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5217. 쌍의 합/쌍의 합.cc
+++ 백준/Bronze/5217. 쌍의 합/쌍의 합.cc
@@ -12,17 +12,15 @@ int main()
         cout << "Pairs for " << n << ": ";
         
         
-        bool comma = false;
         for(int i=1; i<=n/2; ++i)
         {
             if(n == i + (n-i) && i != (n-i))
             {
-                if(comma)
+                if(i != 1)
                 {
                     cout << ", ";
                 }
                 cout << i << ' ' << n-i;
-                comma = true;
             }
         }
         cout << '\n';
