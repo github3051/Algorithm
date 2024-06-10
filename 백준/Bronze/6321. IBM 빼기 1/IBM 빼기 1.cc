5efe75f2ba357555d8aa3168061753a3c5fc7fa5
Regnun <kimzoo3051@naver.com>
2024-06-10 21:58:41 +0900

    [Bronze III] Title: IBM 빼기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/6321. IBM 빼기 1/IBM 빼기 1.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testCases;
+    cin >> testCases;
+    
+    int idx = 1;
+    while(testCases--)
+    {
+        string str;
+        cin >> str;
+        int sLen = str.length();
+        
+        cout << "String #" << idx << '\n';
+        for(int i=0; i<sLen; ++i)
+        {
+            if(str[i] == 'Z')
+            {
+                cout << 'A';
+            }
+            else
+            {
+                cout << (char)(str[i]+1);
+            }
+        }
+        cout << "\n\n";
+        idx += 1;
+    }
+}
