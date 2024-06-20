c13ed62e3a06c34620283d29e5498adc961411e8
Regnun <kimzoo3051@naver.com>
2024-06-20 23:50:55 +0900

    [Bronze IV] Title: 등교, Time: 0 ms, Memory: 2020 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Bronze/31962. 등교/등교.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,x;
+    cin >> n >> x;
+    
+    int ans = 0;
+    while(n--)
+    {
+        int start, end;
+        cin >> start >> end;
+        
+        if(start + end <= x && ans < start)
+        {
+            ans = start;
+        }
+    }
+    cout << ans;
+}
