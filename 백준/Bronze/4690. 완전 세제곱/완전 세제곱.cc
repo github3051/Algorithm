9eb5ce1f1c04b03cfd55c914f8d6febac29e7c64
Regnun <kimzoo3051@naver.com>
2024-06-28 10:37:44 +0900

    [Bronze III] Title: 완전 세제곱, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4690. 완전 세제곱/완전 세제곱.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int a = 6; a <= 100; ++a)
+	{
+		for (int b = 2; b <= 100; ++b)
+		{
+			for (int c = b; c <= 100; ++c)
+			{
+				for (int d = c; d <= 100; ++d)
+				{
+
+					if (a * a * a == b * b * b + c * c * c + d * d * d)
+					{
+						cout << "Cube = " << a << ", Triple = " << '(' << b << ',' << c << ',' << d << ")\n";
+					}
+				}
+			}
+		}
+	}
+}
