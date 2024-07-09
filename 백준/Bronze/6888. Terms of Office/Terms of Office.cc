64317bcd5ee97d32edb87ee93f0aef6ed0f6f0f4
Regnun <kimzoo3051@naver.com>
2024-07-09 21:50:46 +0900

    [Bronze IV] Title: Terms of Office, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6888. Terms of Office/Terms of Office.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    
+    for(int i=x; i<=y; i=i+60)
+    {
+        cout << "All positions change in year " << i << '\n';
+    }
+}
