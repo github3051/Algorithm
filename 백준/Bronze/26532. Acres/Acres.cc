7113b79794860a3945a8b6c2d99b3d6a31a2f513
Regnun <kimzoo3051@naver.com>
2024-07-17 22:04:51 +0900

    [Bronze IV] Title: Acres, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/26532. Acres/Acres.cc
+++ 백준/Bronze/26532. Acres/Acres.cc
@@ -1,21 +1,9 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-
-  int width, length; cin >> width >> length;
-
-  int sqYard = width * length;
-
-  double acreToYard = 4840.0;
-  double cntCorn = 5.0;
-
-  double acres = (double)sqYard / acreToYard;
-  int bags = ceil(acres / cntCorn);
-
-  cout << bags << "\n";
-
-  return 0;
+    int x,y;
+    cin >> x >> y;
+    cout << (x*y + 4840*5 - 1)/(4840*5);
 }
