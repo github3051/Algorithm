7ecaaa2a935117f4ef55e9fb72839ab766c9d6bc
Regnun <kimzoo3051@naver.com>
2024-07-26 20:38:13 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    if(n%2 == 1)
+    {
+        cout << "evoLmoCicS";
+    }
+    else
+    {
+        cout << "SciComLove";
+    }
+}
