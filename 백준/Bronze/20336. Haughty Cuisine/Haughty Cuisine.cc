d38d45b2e10f497262c9d1f13b24bfec044657f7
Regnun <kimzoo3051@naver.com>
2024-09-12 18:45:07 +0900

    [Bronze IV] Title: Haughty Cuisine, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20336. Haughty Cuisine/Haughty Cuisine.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int ansN = 0;
+    vector<string> ansStr;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int d;
+        cin >> d;
+
+        if (ansN < d)
+        {
+            ansN = d;
+            ansStr.clear();
+            for (int j = 0; j < d; ++j)
+            {
+                string sTmp;
+                cin >> sTmp;
+                ansStr.push_back(sTmp);
+            }
+        }
+        else
+        {
+            for (int j = 0; j < d; ++j)
+            {
+                string sTmp;
+                cin >> sTmp;
+            }
+        }
+    }
+
+    cout << ansN << '\n';
+
+    for (const auto& elem : ansStr)
+    {
+        cout << elem << '\n';
+    }
+}
