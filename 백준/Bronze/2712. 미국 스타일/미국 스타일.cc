e26b512ec34d63c39d6fb8a18a56315a7d852b74
Regnun <kimzoo3051@naver.com>
2024-10-13 18:46:37 +0900

    [Bronze III] Title: 미국 스타일, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2712. 미국 스타일/미국 스타일.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		double n;
+		string str;
+		cin >> n >> str;
+		cout <<  fixed << setprecision(4);
+
+		if (str == "kg")
+		{
+			cout << n * 2.2046 << " lb\n";
+		}
+		else if (str == "g")
+		{
+			cout << n * 3.7854 << " l\n";
+		}
+		else if (str == "lb")
+		{
+			cout << n * 0.4536 << " kg\n";
+		}
+		else if (str == "l")
+		{
+			cout << n * 0.2642 << " g\n";
+		}
+	}
+}
