e0ba2a68f45a51b92ad8c89975400f4a68b621c3
Regnun <kimzoo3051@naver.com>
2024-11-05 18:58:13 +0900

    [Bronze III] Title: 백발백준하는 명사수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/22938. 백발백준하는 명사수/백발백준하는 명사수.cc
+++ 백준/Bronze/22938. 백발백준하는 명사수/백발백준하는 명사수.cc
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-	int x1, y1, r1, x2,y2,r2;
+	long long x1, y1, r1, x2,y2,r2;
 	cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
-	double dist = sqrt(pow(x1-x2,2) + pow(y1-y2,2));
-	if (r1 + r2 > dist)
+	long long dist = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
+	if ( (r1 + r2)*(r1 + r2) > dist)
 	{
 		cout << "YES";
 	}
