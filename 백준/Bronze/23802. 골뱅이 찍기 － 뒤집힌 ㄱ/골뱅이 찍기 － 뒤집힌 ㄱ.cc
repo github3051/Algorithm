c86328e81e349803fdac03f2b8b4acf5efbac76e
Regnun <kimzoo3051@naver.com>
2024-11-28 18:41:27 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㄱ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23802. 골뱅이 찍기 － 뒤집힌 ㄱ/골뱅이 찍기 － 뒤집힌 ㄱ.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void Print(const int& row, const int& col)
+{
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < col; ++j)
+        {
+            cout << "@";
+        }
+        cout << '\n';
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    Print(n, n*5);
+    Print(n*4, n);
+}
