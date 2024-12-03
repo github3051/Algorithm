84bea9f36429671ef57148c2c26d652b8b3f2b7d
Regnun <kimzoo3051@naver.com>
2024-12-03 10:44:36 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void PrintWithBlank(const int& row, const int& col)
+{
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < col; ++j)
+        {
+            if (j < row || j >= col - row)
+            {
+                cout << '@';
+            }
+            else
+            {
+                cout << ' ';
+            }
+        }
+        cout << '\n';
+    }
+}
+
+void Print(const int& row, const int& col)
+{
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < col; ++j)
+        {
+            cout << '@';
+        }
+        cout << '\n';
+    }
+}
+
+int main() 
+{
+    int n;
+    cin >> n;
+
+    PrintWithBlank(n, 5 * n);
+    PrintWithBlank(n, 5 * n);
+    Print(n, 5 * n);
+    PrintWithBlank(n, 5 * n);
+    Print(n, 5 * n);
+}
