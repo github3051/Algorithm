090555bdeabbaeb00dd4283c24a580b04bbc91cd
Regnun <kimzoo3051@naver.com>
2024-12-06 06:00:08 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void Print(const int& row, const int& col)
+{
+    for(int i=0; i<row; ++i)
+    {
+        for(int j=0; j<col; ++j)
+        {
+            cout << '@';
+        }
+        cout << '\n';
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    Print(n,n*5);
+    Print(n,n);
+    Print(n,n*5);
+    Print(n,n);
+    Print(n,n*5);    
+}
