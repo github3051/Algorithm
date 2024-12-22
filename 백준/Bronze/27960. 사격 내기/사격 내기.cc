0cd5824d9392ae48103aa0f8d74588a724e56a38
Regnun <kimzoo3051@naver.com>
2024-12-22 18:53:48 +0900

    [Bronze III] Title: 사격 내기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27960. 사격 내기/사격 내기.cc
+++ 백준/Bronze/27960. 사격 내기/사격 내기.cc
@@ -1,37 +1,14 @@
 #include <iostream>
-#include <vector>
+ 
 using namespace std;
-
+ 
+int a, b;
+ 
 int main()
 {
-	vector<int> score = {512,256,128,64,32,16,8,4,2,1};
-	int len = score.size();
-	int scoreA, scoreB;
-	cin >> scoreA >> scoreB;
-
-
-	int ans = 0;
-	for (int i = 0; i < len; ++i)
-	{
-		bool a = false;
-		bool b = false;
-		if (scoreA - score[i] >= 0)
-		{
-			scoreA -= score[i];
-			a = true;
-		}
-
-		if (scoreB - score[i] >= 0)
-		{
-			scoreB -= score[i];
-			b = true;
-		}
-
-		if ((a && !b) || (!a && b))
-		{
-			ans += score[i];
-		}
-	}
-
-	cout << ans;
+    cin >> a >> b;
+    cout << (a ^ b);
+ 
+    return 0;
 }
+ 
