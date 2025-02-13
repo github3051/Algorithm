9a6f5220a39a50d2d19b0cd022fd7f61a7581a57
Regnun <kimzoo3051@naver.com>
2025-02-13 19:37:08 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 100 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+	int t;
+	cin >> t;
+	for (int i = 1; i <= t; ++i)
+	{
+		int n,a,b;
+		cin >> n >> a >> b;
+		for (int j = 0; j < n; ++j)
+		{
+			int tmpA, tmpB;
+			cin >> tmpA >> tmpB;
+		}
+		cout << "Material Management " << i << "\nClassification ---- End!\n";
+	}
+}
