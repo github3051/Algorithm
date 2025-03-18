8201e196b7bca29f707b4aa2bc44faaa0c9ee39f
Regnun <kimzoo3051@naver.com>
2025-03-18 21:57:11 +0900

    [Bronze II] Title: 욱제는 효도쟁이야!!, Time: 16 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> arr[i];
+	}
+	sort(arr.begin(), arr.end());
+	int sum = 0;
+	for (int i = 0; i < n - 1; ++i)
+	{
+		sum += arr[i];
+	}
+	cout << sum;
+}
