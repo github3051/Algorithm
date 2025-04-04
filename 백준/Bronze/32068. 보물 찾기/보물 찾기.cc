5e2bc74834b82bb9f9feec3603ea20bda6aa6ae6
Regnun <kimzoo3051@naver.com>
2025-04-05 00:25:42 +0900

    [Bronze III] Title: 보물 찾기, Time: 276 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/32068. 보물 찾기/보물 찾기.cc
+++ 백준/Bronze/32068. 보물 찾기/보물 찾기.cc
@@ -10,15 +10,6 @@ int main()
 		int l, r, s, ans = 1;
 		cin >> l >> r >> s;
 
-		for (int i = 1; ; ++i)
-		{
-			s = s + i * (i % 2 == 0 ? -1 : 1);
-			ans += 1;
-			if (s == l || s == r)
-			{
-				break;
-			}
-		}
-		cout << ans << '\n';
+		cout << (r - s <= s - l ? 2*(r - s) : 2*(s - l) + 1) << '\n'; // 1 3 5 7 9, 2 4 6 8 10...
 	}
 }
