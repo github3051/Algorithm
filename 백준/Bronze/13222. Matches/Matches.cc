81a1206b5c0f8bfd42e8e449bd45f3fad70a46e9
Regnun <kimzoo3051@naver.com>
2025-05-21 20:09:26 +0900

    [Bronze III] Title: Matches, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/13222. Matches/Matches.cc
+++ 백준/Bronze/13222. Matches/Matches.cc
@@ -6,12 +6,12 @@ int main()
 {
     int n, w, h;
     cin >> n >> w >> h;
-
+    int maxLength = w * w + h *h;
     for (int i = 0; i < n; ++i)
     {
         int match;
         cin >> match;
-        if (match <= (int)sqrt(w * w + h *h))
+        if (match*match <= maxLength)
         {
             cout << "YES\n";
         }
