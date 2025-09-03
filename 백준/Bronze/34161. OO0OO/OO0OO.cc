f8a2089cf711476775043ad2c3566c2a1e2d0570
Regnun <kimzoo3051@naver.com>
2025-09-03 22:56:00 +0900

    [Bronze IV] Title: OO0OO, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/34161. OO0OO/OO0OO.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i = 0; i < 3; ++i)
+	{
+		string str;
+		cin >> str;
+	}
+
+	for (int a = 1; a <= 10; ++a)
+	{
+		for (int b = 1; b <= 10; ++b)
+		{
+			for (int c = 1; c <= 10; ++c)
+			{
+				for (int d = 1; d <= 10; ++d)
+				{
+					cout << -1 << '\n';
+				}
+			}
+		}
+	}
+}
