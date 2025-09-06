2f3523105207acc7706f9e4c78f38ca2bf3e9b8e
Regnun <kimzoo3051@naver.com>
2025-09-07 00:06:15 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	int h = a + c;
+	int y = b + d;
+
+	if (h == y)
+		cout << "Either";
+	else if(h > y)
+		cout << "Yongdap";
+	else if(h < y)
+		cout << "Hanyang Univ.";
+}
